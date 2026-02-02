e371bcc0214e76bc33811138c599971620fddf57
psybr5 <psybr5@nottingham.ac.uk>
2026-02-02 21:55:03 +0000

    basic setup

--- src/sheet-hero/src/App.jsx
+++ src/sheet-hero/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import Sidebar from './Components/Sidebar';
+import PromptInput from './Components/PromptInput';
 
-export default function App() {
-  const [count, setCount] = useState(0)
-
+function App() {
   return (
-    <>
-    <div>
-      
+    <div className="flex h-screen p-2 gap-2">
+      <Sidebar />
+
+      <div className="flex flex-col flex-1 bg-dark_gray rounded-4xl p-2">
+        
+        <div className="flex-1 overflow-y-auto rounded-3xl bg-dark_gray"></div>
+
+        <div className="border-t border-gray p-2">
+          <PromptInput/>
+        </div>
+
+      </div>
     </div>
-    </>
-  )
+  );
 }
+
+export default App;
