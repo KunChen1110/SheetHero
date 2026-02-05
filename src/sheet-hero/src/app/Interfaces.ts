a3946e009d64d20702bf4a5a34639d211d079a86
psybr5 <psybr5@nottingham.ac.uk>
2026-02-05 16:38:44 +0000

    mostly working sidebar

---
+++ src/sheet-hero/src/app/Interfaces.ts
@@ -0,0 +1,26 @@
+export enum Role {
+    USER,
+    ASSISTANT,
+}
+
+
+export interface Chat {
+  id: string;
+  title: string;
+  messages: Message[];
+}
+
+
+export interface Message {
+  id: string;
+  role: Role;
+  content: string;
+}
+
+
+export interface ExcelFile {
+  id: string;
+  name: string;
+  index: number;
+  file: File;
+}
